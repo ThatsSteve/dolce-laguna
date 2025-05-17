330b03c53e7579a6f202bac09e3ada251573b25c
ThatsSteve <rushsteve13@gmail.com>
2025-05-17 15:41:05 +0200

    Fix: sistemato export e array features in spaces.ts

--- app/data/spaces.ts
+++ app/data/spaces.ts
@@ -4,11 +4,11 @@ export const spaces = [
     description: "Iniziate la vostra giornata con una colazione abbondante nella nostra zona colazioni, dove potrete gustare prodotti freschi e locali in un ambiente accogliente.",
     image: "/images/Homepage/Gli_spazi/zona_colazioni.jpg",
     features: [
-      //   "Colazione a buffet con prodotti locali",
-    "Ampia selezione di dolci e pasticcini",
-    "Caffè e tè di alta qualità",
-    "Ambiente luminoso e accogliente",
-    "Vista sul giardino"
+      "Colazione a buffet con prodotti locali",
+      "Ampia selezione di dolci e pasticcini",
+      "Caffè e tè di alta qualità",
+      "Ambiente luminoso e accogliente",
+      "Vista sul giardino"
     ]
-  },
+  }
 ];
